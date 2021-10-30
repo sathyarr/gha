1de05759e7278d016ef211a3c3d906c317075bcc
Sathya R <sathyacse67@gmail.com>
2021-10-30 20:30:13 +0530

    added jquery dependency, Highlighter changes

--- src/pages/Model.tsx
+++ src/pages/Model.tsx
@@ -30,6 +30,7 @@ import BabylonScene from '../components/SceneComponent';
 
 import * as BABYLON from 'babylonjs'
 import * as GUI from 'babylonjs-gui'
+import * as $ from 'jquery'
 import 'babylonjs-loaders'
 import { useEffect, useRef, useState } from 'react';
 import { Scene } from 'babylonjs/scene';
@@ -164,6 +165,125 @@ const Model: React.FC = () => {
             let model = scene.getMeshByName("__root__");
     
             model.rotate(BABYLON.Axis.Y, Math.PI / 2, BABYLON.Space.WORLD);
+
+
+            /**
+             * Sathya Changes: Highlighter Start
+             */
+             var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+             var bodypartToMeshMapping = JSON.parse('{ \
+               "Head": ["node12", "node8"], \
+               "Thigh": ["node14"], \
+               "Hand": ["node18"], \
+               "Spine": ["node6"] \
+             }')
+ 
+             var systemToBodyPartMapping = JSON.parse('{ \
+               "Physical Exam": [ \
+                 { \
+                   "bodyParts": ["Head", "Hand"], \
+                   "neutrality": "positive" \
+                 }, \
+                 { \
+                   "bodyParts": ["Thigh", "Spine"], \
+                   "neutrality": "negative" \
+                 } \
+               ], \
+               "Chief Complaint": [ \
+                 { \
+                   "bodyParts": ["Head"], \
+                   "neutrality": "positive" \
+                 }, \
+                 { \
+                   "bodyParts": ["Thigh", "Hand", "Spine"], \
+                   "neutrality": "negative" \
+                 } \
+               ] \
+             }')
+     
+             function createButton(name: string[], text: string, width: string, height: string, color: string, cornerRadius: number, background: string, alignment: number) {
+               console.log(name.join(), text, width, height, color, cornerRadius, background, alignment)
+     
+               var button = GUI.Button.CreateSimpleButton(name.join() + "btn", text);
+               button.width = width
+               button.height = height;
+               button.color = color;
+               button.cornerRadius = cornerRadius;
+               button.background = background;
+               button.horizontalAlignment = alignment;
+     
+               return button
+             }
+     
+             var bodypartHL = new BABYLON.HighlightLayer("bodypartHL", scene);
+     
+             function highlight(target: string, color: BABYLON.Color3) {
+               let targetMesh = scene.getMeshByName(target);
+               bodypartHL.addMesh(targetMesh, color);
+             }
+ 
+             function highlightHelper(target: any, color: BABYLON.Color3) {
+               $.each(target, function(index, val) {
+                 highlight(val, color);
+               })
+             }
+ 
+             function setBodyPartHighlight(bodyPart: string) {
+               bodypartHL.removeAllMeshes();
+               highlightHelper(bodypartToMeshMapping[bodyPart], BABYLON.Color3.Green());
+             }
+     
+             function createBodyPartPanel() {
+               var bodyPartPanel = new GUI.StackPanel();
+               advancedTexture.addControl(bodyPartPanel);
+ 
+               $.each(bodypartToMeshMapping, function(key: string, val){
+                   var button = createButton(val, key, "150px", "40px", "white", 20, "green", GUI.Control.HORIZONTAL_ALIGNMENT_LEFT)
+                   bodyPartPanel.addControl(button);
+                   button.onPointerUpObservable.add(function() {
+                     setBodyPartHighlight(key);
+                   });
+               });
+             }
+             createBodyPartPanel();
+     
+ 
+             function setSystemHighlight(targets: any) {
+               bodypartHL.removeAllMeshes();
+ 
+               var neutralityToColorMapping = new Map()
+               neutralityToColorMapping.set("positive", BABYLON.Color3.Green())
+               neutralityToColorMapping.set("negative", BABYLON.Color3.Red())
+ 
+               $.each(targets, function(index, val){
+                 // Transform bodypart name to meshname, neutrality key to color
+                 var neutralityValue = val['neutrality']
+                 var meshItems = $.map(val['bodyParts'], function(i) {
+                   return bodypartToMeshMapping[i];
+                 });
+                 highlightHelper(meshItems, neutralityToColorMapping.get(neutralityValue));
+               });
+             }
+     
+             function createSystemPanel() {
+               var panel = new GUI.StackPanel();
+               advancedTexture.addControl(panel);
+               panel.verticalAlignment = 1;
+ 
+               $.each(systemToBodyPartMapping, function(key: string, val){
+                 var button = createButton([key], key, "150px", "40px", "white", 20, "green", GUI.Control.HORIZONTAL_ALIGNMENT_LEFT)
+                 panel.addControl(button);
+                 button.onPointerUpObservable.add(function() {
+                   setSystemHighlight(val);
+                 });
+               });
+             }
+             createSystemPanel();
+            /**
+             * Sathya Changes: Highlighter End
+             */
+
             scene.createDefaultEnvironment({
             createSkybox: false,
             createGround: false
